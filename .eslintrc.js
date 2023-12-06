c6c93ed0a78e3e337cf1cd2834b1338838cf068e
verhovensky <voinywolnyprod@gmail.com>
2023-12-06 22:55:50 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  // Prettier config
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  // Ignore import React from react in js/ts files
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+  ],
 };
